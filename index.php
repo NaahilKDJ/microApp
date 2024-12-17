109f3f23cc56eeefeb35580ea7d522c27e9b6325
NaahilKDJ <134393859+NaahilKDJ@users.noreply.github.com>
2024-12-17 15:02:02 +0100

    commit base

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include "html/header.html";
+?>
+<body>
+    <form action="">
+        <label for="">
+            <input type="text" name="nb">
+        </label>
+        <label for="">
+            <input type="text" name="nbA">
+        </label>
+
+        <input type="submit" name="action" value="Additionner">
+    </form>
+
+
+<?php
+include "html/footer.html";
+?>
